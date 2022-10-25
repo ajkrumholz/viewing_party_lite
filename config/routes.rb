23fff7626d992add748493eaabb81fcb7c84e981
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-25 17:48:43 -0400

    trim down routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   get "/login", to: "login#index"
   post "/login", to: "login#create"
-  resource :user, path: 'dashboard' do
+  resource :user, path: 'dashboard', only: %i[create show] do
     post "/movies_top_rated", to: "movies#top_rated"
     post "/movies_search", to: "movies#search"
     resources :discover, only: [:index], module: "users"
